daf27f79e2cda70be0aec6f163983384c3eafef2
k-saito-en <saito.kotaro.engineer@gmail.com>
2022-11-19 16:11:31 +0900

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 齊藤虎太郎 on 2022/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
